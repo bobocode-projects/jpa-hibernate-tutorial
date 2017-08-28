22a78e6e074ed7b8316bae203e4aeab3358f07a1
Andrii Kulishov <akulishov@playtika.com>
2017-08-28 17:41:58 +0300

    Some shit added

--- jpa-hibernate-servlet-task/src/main/java/com/bobocode/dao/impl/AccountDaoImpl.java
+++ jpa-hibernate-servlet-task/src/main/java/com/bobocode/dao/impl/AccountDaoImpl.java
@@ -6,11 +6,12 @@ import com.bobocode.model.Account;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import java.util.List;
+import java.util.function.Function;
 
 
 public class AccountDaoImpl implements AccountDao {
 
-    private final EntityManagerFactory emf;
+    private EntityManagerFactory emf;
 
     public AccountDaoImpl(EntityManagerFactory emf) {
         this.emf = emf;
@@ -21,31 +22,60 @@ public class AccountDaoImpl implements AccountDao {
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
 
-        //todo: find account by id using EntityManager
+        Account account = em.find(Account.class, id);
 
         em.getTransaction().commit();
         em.close();
 
-        throw new UnsupportedOperationException("Method is not implemented yet. It's your homework!");
-        // todo: return account
+        return account;
     }
 
     @Override
     public Account findByEmail(String email) {
-        // todo: implement search by email via EntityManager
-        throw new UnsupportedOperationException("Method is not implemented yet. It's your homework!");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        Account account = em.createQuery("SELECT a FROM Account a WHERE a.email = :email", Account.class).getSingleResult();
+
+        em.getTransaction().commit();
+        em.close();
+
+        return account;
     }
 
     @Override
     public List<Account> findAll() {
-        //todo: find and return all accounts using EntityManagers
-        throw new UnsupportedOperationException("Method is not implemented yet. It's your homework!");
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        List<Account> accounts = em.createQuery("SELECT a FROM Account a WHERE a.email = :email", Account.class).getResultList();
+
+        em.getTransaction().commit();
+        em.close();
+
+        return accounts;
     }
 
     @Override
     public void save(Account account) {
-        // todo: save an account sing EntityManager
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
+
+        em.persist(account);
+
+        em.getTransaction().commit();
+        em.close();
     }
 
+    private <R> List<R> performWithinPersistence(Function<EntityManager, List<R>> operation) {
+        EntityManager em = emf.createEntityManager();
+        em.getTransaction().begin();
 
+        List<R> results = operation.apply(em);
+
+        em.getTransaction().commit();
+        em.close();
+
+        return results;
+    }
 }
